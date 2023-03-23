c6ed437b332901a0dc812b60b34b6546c1b29695
PERENNOU Tanguy <tanguy.perennou@isae.fr>
2023-03-23 21:19:45 +0100

    Add structural comments to views

--- chinook/disks/views.py
+++ chinook/disks/views.py
@@ -5,6 +5,10 @@ from .forms import SearchForm
 from .models import Album
 
 def album_list(request):
+    """
+    Get albums from database, either all of them or those matching a POST request
+    :param request: The incoming request
+    """
     if request.method == 'POST':
         search_form = SearchForm(request.POST)
         if search_form.is_valid():
@@ -18,5 +22,10 @@ def album_list(request):
     return render(request, 'disks/album_list.html', {'albums': albums, 'form': search_form})
 
 def album_details(request, album_id):
+    """
+    Get the specified album
+    :param request: The incoming request
+    :param album_id: The album's ID
+    """
     album = get_object_or_404(Album, pk=album_id)
     return render(request, 'disks/album_details.html', {'album': album})
